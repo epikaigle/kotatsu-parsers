c30da3cf1f73fdeb3aca5f3ef32bf987558dd5a9
dragonx943 <premieregirl26@gmail.com>
2026-02-16 09:35:04 +0700

    fix(HentaixComic): Invalid requests for fetching the chapters list

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/HentaixComic.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/HentaixComic.kt
@@ -8,6 +8,4 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("HENTAIXCOMIC", "Hentai x Comic", "en", ContentType.HENTAI)
 internal class HentaixComic(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.HENTAIXCOMIC, "hentaixcomic.com", 16) {
-	override val postReq = true
-}
+	MadaraParser(context, MangaParserSource.HENTAIXCOMIC, "hentaixcomic.com", 16)
