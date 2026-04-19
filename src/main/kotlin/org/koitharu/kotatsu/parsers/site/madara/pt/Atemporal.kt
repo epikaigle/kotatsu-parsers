781854e12bd4260773e13ade248fa13bafdf365d
Finlay Hansford <144564801+AgentKush@users.noreply.github.com>
2026-04-19 07:26:37 +0100

    fix(pt/atemporal): mark @Broken - domain offline, no successor (#252)

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Atemporal.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/pt/Atemporal.kt
@@ -1,10 +1,12 @@
 package org.koitharu.kotatsu.parsers.site.madara.pt
 
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
+@Broken // atemporal.cloud is offline (connection refused) and no successor domain has surfaced publicly.
 @MangaSourceParser("ATEMPORAL", "Atemporal", "pt")
 internal class Atemporal(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.ATEMPORAL, "atemporal.cloud") {
